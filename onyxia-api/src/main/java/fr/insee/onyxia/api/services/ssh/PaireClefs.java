6953fdfae5afbd719c46d2e6fc0663ec22981ab0
Olivier Levitt <olivier.levitt@insee.fr>
2020-03-26 21:19:01 +0100

    Update dependencies (#8)

--- onyxia-api/src/main/java/fr/insee/onyxia/api/services/ssh/PaireClefs.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/services/ssh/PaireClefs.java
@@ -1,36 +1,9 @@
 package fr.insee.onyxia.api.services.ssh;
 
-import com.google.api.client.repackaged.com.google.common.base.Objects;
-import com.google.common.base.MoreObjects;
-
 public class PaireClefs {
 	String privateKey;
 	String publicKey;
 	
-	@Override
-	public int hashCode() {
-		return Objects.hashCode(this.privateKey + this.publicKey);
-	}
-
-	@Override
-	public boolean equals(Object object) {
-		if (object == null) {
-			return false;
-		}
-		if (!(object instanceof PaireClefs)) {
-			return false;
-		}
-		final PaireClefs other = (PaireClefs) object;
-		return Objects.equal(this.privateKey, other.privateKey) && Objects.equal(this.publicKey, other.publicKey);
-	}
-
-	@Override
-	public String toString() {
-		return MoreObjects.toStringHelper(PaireClefs.class)
-					      .add("Clef privée : ", this.privateKey)
-					      .add("\nClef publique : ", this.publicKey)
-					      .toString();
-	}
 
 	public void setPrivateKey(String key) {
 		this.privateKey = key;
