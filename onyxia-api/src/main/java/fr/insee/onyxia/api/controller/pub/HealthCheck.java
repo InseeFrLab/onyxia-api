38d1ab16597ad77709e527552e1be916c84a1ffb
Olivier Levitt <ungawa14@gmail.com>
2020-04-29 15:49:47 +0200

    WIP : add build info

--- onyxia-api/src/main/java/fr/insee/onyxia/api/controller/pub/HealthCheck.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/controller/pub/HealthCheck.java
@@ -1,19 +1,23 @@
 package fr.insee.onyxia.api.controller.pub;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @Tag(name="Public")
 @RequestMapping("/public")
 public class HealthCheck {
 
+    @Autowired
+    BuildProperties buildProperties;
+
     @GetMapping("/healthcheck")
     public String healthcheck() {
-       return null;
+       return buildProperties.getTime().toString();
     }
     
 }
