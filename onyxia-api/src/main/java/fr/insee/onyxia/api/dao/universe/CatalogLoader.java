2555b8caa00ff7e5c1343a00ee27f0c7fc910c44
Olivier Levitt <ungawa14@gmail.com>
2020-05-15 15:26:31 +0200

    Cleanup

--- onyxia-api/src/main/java/fr/insee/onyxia/api/dao/universe/CatalogLoader.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/dao/universe/CatalogLoader.java
@@ -68,18 +68,6 @@ public class CatalogLoader {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        // Property friendlyName = new Property();
-        // friendlyName.setType("string");
-        // friendlyName.setDescription("Nom d'affichage du service sur Onyxia");
-        // friendlyName.setDefaut(pkg.getName());
-        // friendlyName.setTitle("Un titre plus sympathique?");
-        // Category onyxia = new Category();
-        // Map<String, Property> map = new HashMap<String, Property>();
-        // map.put("friendly_name", friendlyName);
-        // onyxia.setProperties(map);
-        // onyxia.setType("object");
-        // onyxia.setDescription("Configure l'ensemble des metas-donnees pour Onyxia");
-        // pkg.getProperties().getCategories().put("onyxia", onyxia);
     }
 
     /**
