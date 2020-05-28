80fb977cb0bc1eb0da7874a0f281c86d432ec2bf
Olivier Levitt <ungawa14@gmail.com>
2020-05-28 15:43:52 +0200

    Setup kotlin

--- onyxia-api/src/main/java/fr/insee/onyxia/mustache/Mustacheur.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/mustache/Mustacheur.java
@@ -1,15 +1,17 @@
 package fr.insee.onyxia.mustache;
 
-import java.io.StringReader;
-import java.io.StringWriter;
-
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
+import com.mixed.kotlin.Person;
+
+import java.io.StringReader;
+import java.io.StringWriter;
 
 public class Mustacheur {
 
 	public static String mustache(String json, Object object) throws Exception {
+		Person person = null;
 		MustacheFactory mf = new DefaultMustacheFactory();
 		Mustache mustache = mf.compile(new StringReader(json),"marathon.json.mustache");
 		StringWriter writer = new StringWriter();
---
+++ onyxia-api/src/main/kotlin/Person.kt
@@ -0,0 +1,11 @@
+package com.mixed.kotlin
+import fr.insee.onyxia.model.User
+
+data class Person(val name: String) {
+    var age: Int = 0
+    var user: User? = null;
+
+    override fun toString(): String {
+        return super.toString()
+    }
+}
