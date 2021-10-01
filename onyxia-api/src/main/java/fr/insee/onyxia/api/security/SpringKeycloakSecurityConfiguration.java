d9b384541fd8cf969af15b7602add88ea5771744
Olivier Levitt <olivier.levitt@gmail.com>
2021-10-01 17:23:37 +0200

    Fix import

--- onyxia-api/src/main/java/fr/insee/onyxia/api/security/SpringKeycloakSecurityConfiguration.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/security/SpringKeycloakSecurityConfiguration.java
@@ -31,6 +31,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.ArrayList;
 
 public class SpringKeycloakSecurityConfiguration {
    
