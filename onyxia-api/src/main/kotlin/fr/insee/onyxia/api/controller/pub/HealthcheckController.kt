9f2da1345857cf0f72acce479b47b3a5aadad49d
Mathieu Parent <math.parent@gmail.com>
2021-11-30 10:33:13 +0100

    Fix Kotlin array

--- onyxia-api/src/main/kotlin/fr/insee/onyxia/api/controller/pub/HealthcheckController.kt
+++ onyxia-api/src/main/kotlin/fr/insee/onyxia/api/controller/pub/HealthcheckController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @Tag(name = "Public")
-@RequestMapping({"/api/public", "/public"})
+@RequestMapping(["/api/public", "/public"])
 class HealthcheckController {
 
     @GetMapping("/healthcheck")
