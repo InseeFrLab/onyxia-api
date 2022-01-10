81cd8733ef4d562f41007cc9575dc610fc9ac4d8
Olivier Levitt <olivier.levitt@gmail.com>
2022-01-10 18:39:13 +0100

    Fix no auth mode

--- onyxia-api/src/main/java/fr/insee/onyxia/api/security/KeycloakUserProvider.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/security/KeycloakUserProvider.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
+@ConditionalOnProperty(name = "authentication.mode", havingValue = "openidconnect")
 public class KeycloakUserProvider {
 
     @Autowired
