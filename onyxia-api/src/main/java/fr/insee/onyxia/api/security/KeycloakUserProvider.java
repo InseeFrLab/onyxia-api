5177a57ac84e9a1f994094d5f0bc97ae9113086a
Olivier Levitt <olivier.levitt@gmail.com>
2022-01-10 18:41:45 +0100

    Fix missing import

--- onyxia-api/src/main/java/fr/insee/onyxia/api/security/KeycloakUserProvider.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/security/KeycloakUserProvider.java
@@ -6,6 +6,7 @@ import fr.insee.onyxia.model.User;
 import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.representations.AccessToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
