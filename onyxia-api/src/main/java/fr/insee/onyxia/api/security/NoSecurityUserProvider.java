d4bf3f806e7f0e7c6081bbc72c943d899cebd623
Olivier Levitt <olivier.levitt@gmail.com>
2022-01-18 17:06:41 +0100

    Fix injection

--- onyxia-api/src/main/java/fr/insee/onyxia/api/security/NoSecurityUserProvider.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/security/NoSecurityUserProvider.java
@@ -3,21 +3,12 @@ package fr.insee.onyxia.api.security;
 import fr.insee.onyxia.api.services.UserProvider;
 import fr.insee.onyxia.api.services.utils.HttpRequestUtils;
 import fr.insee.onyxia.model.User;
-import org.keycloak.KeycloakSecurityContext;
-import org.keycloak.representations.AccessToken;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
-import org.springframework.context.annotation.*;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class NoSecurityUserProvider {
@@ -26,7 +17,7 @@ public class NoSecurityUserProvider {
     private HttpRequestUtils httpRequestUtils;
 
     @Bean
-    @ConditionalOnMissingBean(UserProvider.class)
+    @Primary
     public UserProvider getUserProvider() {
         return () -> {
             User user = User.newInstance()
