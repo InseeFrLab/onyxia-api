3ab0e6ca8613740f44b84bd9b8475a34c9f532cf
Olivier Levitt <olivier.levitt@insee.fr>
2022-01-18 17:48:33 +0100

    Fix bean injection (#114)

--- onyxia-api/src/main/java/fr/insee/onyxia/api/security/NoSecurityConfiguration.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/security/NoSecurityConfiguration.java
@@ -1,22 +1,17 @@
 package fr.insee.onyxia.api.security;
 
-import fr.insee.onyxia.api.services.UserProvider;
 import fr.insee.onyxia.api.services.utils.HttpRequestUtils;
-import fr.insee.onyxia.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
 @EnableWebSecurity
-@ConditionalOnSingleCandidate(WebSecurityConfigurerAdapter.class)
+@ConditionalOnExpression("'${authentication.mode}' == 'none' or '${authentication.mode}' == ''")
 public class NoSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
--- onyxia-api/src/main/java/fr/insee/onyxia/api/security/NoSecurityUserProvider.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/security/NoSecurityUserProvider.java
@@ -3,30 +3,21 @@ package fr.insee.onyxia.api.security;
 import fr.insee.onyxia.api.services.UserProvider;
 import fr.insee.onyxia.api.services.utils.HttpRequestUtils;
 import fr.insee.onyxia.model.User;
-import org.keycloak.KeycloakSecurityContext;
-import org.keycloak.representations.AccessToken;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
-import org.springframework.context.annotation.*;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
-
 @Configuration
+@ConditionalOnExpression("'${authentication.mode}' == 'none' or '${authentication.mode}' == ''")
 public class NoSecurityUserProvider {
 
     @Autowired
     private HttpRequestUtils httpRequestUtils;
 
     @Bean
-    @ConditionalOnMissingBean(UserProvider.class)
     public UserProvider getUserProvider() {
         return () -> {
             User user = User.newInstance()
