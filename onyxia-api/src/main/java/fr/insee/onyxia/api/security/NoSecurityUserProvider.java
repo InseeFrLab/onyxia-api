5c384600dea2714e95d695c7c2023b371f5cb9b8
Olivier Levitt <olivier.levitt@gmail.com>
2022-01-18 17:42:35 +0100

    Fix injection

--- onyxia-api/src/main/java/fr/insee/onyxia/api/security/NoSecurityUserProvider.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/security/NoSecurityUserProvider.java
@@ -4,20 +4,20 @@ import fr.insee.onyxia.api.services.UserProvider;
 import fr.insee.onyxia.api.services.utils.HttpRequestUtils;
 import fr.insee.onyxia.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
+@ConditionalOnExpression("'${authentication.mode}' == 'none' or '${authentication.mode}' == ''")
 public class NoSecurityUserProvider {
 
     @Autowired
     private HttpRequestUtils httpRequestUtils;
 
     @Bean
-    @ConditionalOnMissingBean
     public UserProvider getUserProvider() {
         return () -> {
             User user = User.newInstance()
