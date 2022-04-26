c53ee6d15aa49739f6f62daebe2c24972288923f
Alexis Dondon <63847345+alexisdondon@users.noreply.github.com>
2022-04-26 16:54:53 +0200

    Add user authentication mode (#122)

--- onyxia-api/src/main/java/fr/insee/onyxia/api/configuration/kubernetes/HelmClientProvider.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/configuration/kubernetes/HelmClientProvider.java
@@ -1,51 +1,56 @@
 package fr.insee.onyxia.api.configuration.kubernetes;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import fr.insee.onyxia.api.configuration.SecurityConfig;
 import fr.insee.onyxia.model.User;
 import fr.insee.onyxia.model.region.Region;
 import io.github.inseefrlab.helmwrapper.configuration.HelmConfiguration;
 import io.github.inseefrlab.helmwrapper.service.HelmInstallService;
 import io.github.inseefrlab.helmwrapper.service.HelmRepoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelmClientProvider {
 
     @Bean
     public HelmRepoService defaultHelmRepoService() {
-        return new HelmRepoService();
+	return new HelmRepoService();
     }
 
     @Bean
     public HelmInstallService defaultHelmInstallService() {
-        return new HelmInstallService();
+	return new HelmInstallService();
     }
 
     @Autowired
-    private SecurityConfig securityConfig = new SecurityConfig();
+    private final SecurityConfig securityConfig = new SecurityConfig();
 
 
     public HelmConfiguration getConfiguration(Region region, User user) {
-        HelmConfiguration helmConfiguration = new HelmConfiguration();
-        if (region.getServices().getServer() != null) {
-            Region.Auth auth = region.getServices().getServer().getAuth();
-            if (auth != null) {
-                helmConfiguration.setKubeToken(auth.getToken());
-            }
-            helmConfiguration.setApiserverUrl(region.getServices().getServer().getUrl());
-            helmConfiguration.setKubeConfig(null);
-        }
-        String username = user.getIdep();
-        if (region.getServices().getUsernamePrefix() != null) {
-            username = region.getServices().getUsernamePrefix()+username;
-        }
+	final HelmConfiguration helmConfiguration = new HelmConfiguration();
+	if (region.getServices().getServer() != null) {
+	    final Region.Auth auth = region.getServices().getServer().getAuth();
+	    if (auth != null) {
+		helmConfiguration.setKubeToken(auth.getToken());
+	    }
+	    helmConfiguration.setApiserverUrl(region.getServices().getServer().getUrl());
+	    helmConfiguration.setKubeConfig(null);
+	}
+	String username = user.getIdep();
+	if (region.getServices().getUsernamePrefix() != null) {
+	    username = region.getServices().getUsernamePrefix()+username;
+	}
 
-        if (region.getServices().getAuthenticationMode() == Region.Services.AuthenticationMode.IMPERSONATE) {
-            helmConfiguration.setAsKubeUser(username);
-        }
+	if (region.getServices().getAuthenticationMode() == Region.Services.AuthenticationMode.IMPERSONATE) {
+	    helmConfiguration.setAsKubeUser(username);
+	}
 
-        return helmConfiguration;
+	if (region.getServices().getAuthenticationMode() == Region.Services.AuthenticationMode.USER) {
+	    helmConfiguration.setKubeToken((String) user.getAttributes().get("access_token"));
+	}
+
+	return helmConfiguration;
     }
 }
