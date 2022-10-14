a77867c8e6be6a234876dca11b7b262bf3b0627a
fcomte <frederic.comte@insee.fr>
2022-10-14 14:05:08 +0000

    user defined values

--- onyxia-api/src/main/java/fr/insee/onyxia/api/controller/pub/CatalogController.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/controller/pub/CatalogController.java
@@ -175,6 +175,12 @@ public class CatalogController {
       customName.setDefaut(pkg.getName());
       customName.setTitle("Custom name");
       onyxiaProperties.put("friendlyName", customName);
+      Property userDefinedValues = new Property();
+      userDefinedValues.setType("string");
+      userDefinedValues.setDescription("Values defined by the end user");
+      userDefinedValues.setDefaut(pkg.getName());
+      userDefinedValues.setTitle("User defined values");
+      onyxiaProperties.put("userDefinedValues", userDefinedValues);      
       Property owner = new Property();
       owner.setType("string");
       owner.setDescription("Owner of the chart");
