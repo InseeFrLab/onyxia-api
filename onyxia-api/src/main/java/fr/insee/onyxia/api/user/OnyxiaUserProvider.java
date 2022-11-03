ed9f72a715b76828faa6569a59fc0a903d788135
Olivier Levitt <olivier.levitt@gmail.com>
2022-11-03 16:50:18 +0100

    Fix logi

--- onyxia-api/src/main/java/fr/insee/onyxia/api/user/OnyxiaUserProvider.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/user/OnyxiaUserProvider.java
@@ -22,18 +22,20 @@ public class OnyxiaUserProvider {
         OnyxiaUser user = new OnyxiaUser(userProvider.getUser(region));
 
         Project userProject = getUserProject(region, user);
-        user.getProjects().add(userProject);
+        if (region.getServices().isSingleNamespace() || region.getServices().isUserNamespace()) {
+            user.getProjects().add(userProject);
+        }
 
         if (!region.getServices().isSingleNamespace()) {
             userProvider.getUser(region).getGroups().stream().forEach(group -> {
                 Project project = new Project();
-                project.setId(region.getServices().getGroupNamespacePrefix()+group);
+                project.setId(region.getServices().getGroupNamespacePrefix() + group);
                 project.setGroup(group);
-                project.setVaultTopDir(region.getServices().getGroupNamespacePrefix()+group);
-                if(region.getData()!=null && region.getData().getS3()!=null){
-                    project.setBucket(region.getData().getS3().getGroupBucketPrefix()+group);
+                project.setVaultTopDir(region.getServices().getGroupNamespacePrefix() + group);
+                if (region.getData() != null && region.getData().getS3() != null) {
+                    project.setBucket(region.getData().getS3().getGroupBucketPrefix() + group);
                 }
-                project.setNamespace(region.getServices().getGroupNamespacePrefix()+group);
+                project.setNamespace(region.getServices().getGroupNamespacePrefix() + group);
                 user.getProjects().add(project);
             });
         }
@@ -41,29 +43,26 @@ public class OnyxiaUserProvider {
         return user;
     }
 
-    private Project getUserProject(Region region, OnyxiaUser user ) {
+    private Project getUserProject(Region region, OnyxiaUser user) {
         Project userProject = new Project();
         if (region.getServices().isSingleNamespace()) {
             userProject.setId("single-project");
             userProject.setGroup(null);
             userProject.setVaultTopDir(user.getUser().getIdep());
-            if(region.getData()!=null && region.getData().getS3()!=null){
-                userProject.setBucket(region.getData().getS3().getBucketPrefix()+user.getUser().getAttributes().get(region.getData().getS3().getBucketClaim()));
+            if (region.getData() != null && region.getData().getS3() != null) {
+                userProject.setBucket(region.getData().getS3().getBucketPrefix() + user.getUser().getAttributes().get(region.getData().getS3().getBucketClaim()));
             }
             userProject.setNamespace(kubernetesService.getCurrentNamespace(region));
             userProject.setName("Single namespace, single project");
-        }
-        else {
-            if (region.getServices().isUserNamespace()) {
-                userProject.setId(region.getServices().getNamespacePrefix()+user.getUser().getIdep());
-                userProject.setGroup(null);
-                userProject.setVaultTopDir(user.getUser().getIdep());
-                if(region.getData()!=null && region.getData().getS3()!=null){
-                    userProject.setBucket(region.getData().getS3().getBucketPrefix()+user.getUser().getAttributes().get(region.getData().getS3().getBucketClaim()));
-                }
-                userProject.setNamespace(region.getServices().getNamespacePrefix()+user.getUser().getIdep());
-                userProject.setName(user.getUser().getIdep()+" personal project");
+        } else {
+            userProject.setId(region.getServices().getNamespacePrefix() + user.getUser().getIdep());
+            userProject.setGroup(null);
+            userProject.setVaultTopDir(user.getUser().getIdep());
+            if (region.getData() != null && region.getData().getS3() != null) {
+                userProject.setBucket(region.getData().getS3().getBucketPrefix() + user.getUser().getAttributes().get(region.getData().getS3().getBucketClaim()));
             }
+            userProject.setNamespace(region.getServices().getNamespacePrefix() + user.getUser().getIdep());
+            userProject.setName(user.getUser().getIdep() + " personal project");
         }
         return userProject;
     }
