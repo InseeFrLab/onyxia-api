9e0fb3232a0b516d2055dc75f2dcdab656df57ae
Olivier Levitt <olivier.levitt@gmail.com>
2022-11-16 16:50:34 +0100

    Disable compatibility checks for tests

---
+++ onyxia-api/src/test/java/fr/insee/onyxia/api/configuration/checks/DisableChecksForTests.java
@@ -0,0 +1,19 @@
+package fr.insee.onyxia.api.configuration.checks;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DisableChecksForTests {
+
+    @MockBean
+    CompatibilityChecks compatibilityChecks;
+
+    public CompatibilityChecks getCompatibilityChecks() {
+        return compatibilityChecks;
+    }
+
+    public void setCompatibilityChecks(CompatibilityChecks compatibilityChecks) {
+        this.compatibilityChecks = compatibilityChecks;
+    }
+}
