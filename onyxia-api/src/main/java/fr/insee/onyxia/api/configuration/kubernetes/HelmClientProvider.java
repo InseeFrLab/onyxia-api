0263bfafc2ea41bddd18bbb79f49f7a9d56e9174
Olivier Levitt <olivier.levitt@gmail.com>
2022-11-16 17:29:51 +0100

    Cleanup kube dependencies (#178)

--- onyxia-api/src/main/java/fr/insee/onyxia/api/configuration/kubernetes/HelmClientProvider.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/configuration/kubernetes/HelmClientProvider.java
@@ -1,56 +1,60 @@
 package fr.insee.onyxia.api.configuration.kubernetes;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import fr.insee.onyxia.api.configuration.SecurityConfig;
 import fr.insee.onyxia.model.User;
 import fr.insee.onyxia.model.region.Region;
 import io.github.inseefrlab.helmwrapper.configuration.HelmConfiguration;
 import io.github.inseefrlab.helmwrapper.service.HelmInstallService;
 import io.github.inseefrlab.helmwrapper.service.HelmRepoService;
+import io.github.inseefrlab.helmwrapper.service.HelmVersionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelmClientProvider {
 
+    @Autowired
+    private final SecurityConfig securityConfig = new SecurityConfig();
+
     @Bean
     public HelmRepoService defaultHelmRepoService() {
-	return new HelmRepoService();
+        return new HelmRepoService();
     }
 
     @Bean
     public HelmInstallService defaultHelmInstallService() {
-	return new HelmInstallService();
+        return new HelmInstallService();
     }
 
-    @Autowired
-    private final SecurityConfig securityConfig = new SecurityConfig();
-
+    @Bean
+    public HelmVersionService defaultHelmVersionService() {
+        return new HelmVersionService();
+    }
 
     public HelmConfiguration getConfiguration(Region region, User user) {
-	final HelmConfiguration helmConfiguration = new HelmConfiguration();
-	if (region.getServices().getServer() != null) {
-	    final Region.Auth auth = region.getServices().getServer().getAuth();
-	    if (auth != null) {
-		helmConfiguration.setKubeToken(auth.getToken());
-	    }
-	    helmConfiguration.setApiserverUrl(region.getServices().getServer().getUrl());
-	    helmConfiguration.setKubeConfig(null);
-	}
-	String username = user.getIdep();
-	if (region.getServices().getUsernamePrefix() != null) {
-	    username = region.getServices().getUsernamePrefix()+username;
-	}
+        final HelmConfiguration helmConfiguration = new HelmConfiguration();
+        if (region.getServices().getServer() != null) {
+            final Region.Auth auth = region.getServices().getServer().getAuth();
+            if (auth != null) {
+                helmConfiguration.setKubeToken(auth.getToken());
+            }
+            helmConfiguration.setApiserverUrl(region.getServices().getServer().getUrl());
+            helmConfiguration.setKubeConfig(null);
+        }
+        String username = user.getIdep();
+        if (region.getServices().getUsernamePrefix() != null) {
+            username = region.getServices().getUsernamePrefix() + username;
+        }
 
-	if (region.getServices().getAuthenticationMode() == Region.Services.AuthenticationMode.IMPERSONATE) {
-	    helmConfiguration.setAsKubeUser(username);
-	}
+        if (region.getServices().getAuthenticationMode() == Region.Services.AuthenticationMode.IMPERSONATE) {
+            helmConfiguration.setAsKubeUser(username);
+        }
 
-	if (region.getServices().getAuthenticationMode() == Region.Services.AuthenticationMode.USER) {
-	    helmConfiguration.setKubeToken((String) user.getAttributes().get("access_token"));
-	}
+        if (region.getServices().getAuthenticationMode() == Region.Services.AuthenticationMode.USER) {
+            helmConfiguration.setKubeToken((String) user.getAttributes().get("access_token"));
+        }
 
-	return helmConfiguration;
+        return helmConfiguration;
     }
 }
--- onyxia-api/src/main/java/fr/insee/onyxia/api/configuration/kubernetes/KubernetesClientProvider.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/configuration/kubernetes/KubernetesClientProvider.java
@@ -1,18 +1,17 @@
 package fr.insee.onyxia.api.configuration.kubernetes;
 
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-
 import fr.insee.onyxia.api.configuration.SecurityConfig;
 import fr.insee.onyxia.model.User;
 import fr.insee.onyxia.model.region.Region;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KubernetesClientProvider {
@@ -30,56 +29,56 @@ public class KubernetesClientProvider {
      * @return
      */
     public KubernetesClient getRootClient(Region region) {
-	final Config config = getDefaultConfiguration(region).build();
-	return new DefaultKubernetesClient(config);
+        final Config config = getDefaultConfiguration(region).build();
+        return new KubernetesClientBuilder().withConfig(config).build();
     }
 
     public KubernetesClient getUserClient(Region region, User user) {
-	final Config config = getDefaultConfiguration(region).build();
-	String username = user.getIdep();
-	if (region.getServices().getUsernamePrefix() != null) {
-	    username = region.getServices().getUsernamePrefix() + user.getIdep();
-	}
+        final Config config = getDefaultConfiguration(region).build();
+        String username = user.getIdep();
+        if (region.getServices().getUsernamePrefix() != null) {
+            username = region.getServices().getUsernamePrefix() + user.getIdep();
+        }
 
-	if (region.getServices().getAuthenticationMode() == Region.Services.AuthenticationMode.IMPERSONATE) {
-	    config.setImpersonateUsername(username);
-	    config.setImpersonateGroups(null);
-	}
+        if (region.getServices().getAuthenticationMode() == Region.Services.AuthenticationMode.IMPERSONATE) {
+            config.setImpersonateUsername(username);
+            config.setImpersonateGroups(null);
+        }
 
-	if (region.getServices()
-		.getAuthenticationMode() == Region.Services.AuthenticationMode.USER) {
-	    config.setOauthToken((String) user.getAttributes().get("access_token"));
-	}
+        if (region.getServices()
+                .getAuthenticationMode() == Region.Services.AuthenticationMode.USER) {
+            config.setOauthToken((String) user.getAttributes().get("access_token"));
+        }
 
 
-	return new DefaultKubernetesClient(config);
+        return new KubernetesClientBuilder().withConfig(config).build();
     }
 
     private ConfigBuilder getDefaultConfiguration(Region region) {
-	final ConfigBuilder configBuilder = new ConfigBuilder();
-	if (region.getServices().getServer() != null
-		&& region.getServices().getServer().getUrl() != null) {
-	    configBuilder.withMasterUrl(region.getServices()
-		    .getServer()
-		    .getUrl());
-	}
+        final ConfigBuilder configBuilder = new ConfigBuilder();
+        if (region.getServices().getServer() != null
+                && region.getServices().getServer().getUrl() != null) {
+            configBuilder.withMasterUrl(region.getServices()
+                    .getServer()
+                    .getUrl());
+        }
 
-	if (region.getServices()
-		.getServer() != null) {
-	    final Region.Auth auth = region.getServices()
-		    .getServer()
-		    .getAuth();
-	    if (auth != null) {
-		if (StringUtils.isNotEmpty(auth.getToken())) {
-		    configBuilder.withOauthToken(auth.getToken());
-		}
-		if (StringUtils.isNotEmpty(auth.getUsername())
-			&& StringUtils.isNotEmpty(auth.getPassword())) {
-		    configBuilder.withUsername(auth.getUsername());
-		    configBuilder.withPassword(auth.getPassword());
-		}
-	    }
-	}
-	return configBuilder;
+        if (region.getServices()
+                .getServer() != null) {
+            final Region.Auth auth = region.getServices()
+                    .getServer()
+                    .getAuth();
+            if (auth != null) {
+                if (StringUtils.isNotEmpty(auth.getToken())) {
+                    configBuilder.withOauthToken(auth.getToken());
+                }
+                if (StringUtils.isNotEmpty(auth.getUsername())
+                        && StringUtils.isNotEmpty(auth.getPassword())) {
+                    configBuilder.withUsername(auth.getUsername());
+                    configBuilder.withPassword(auth.getPassword());
+                }
+            }
+        }
+        return configBuilder;
     }
 }
