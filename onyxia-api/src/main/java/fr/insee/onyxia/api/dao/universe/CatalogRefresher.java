fa3791a8bc6845fbb765da4bdca6f0b19ccdbdcb
CChemin <cecile.chemin@insee.fr>
2022-12-12 20:33:16 +0100

    Fix excluded charts are downloaded for entries
    Signed-off-by: CChemin <cecile.chemin@insee.fr>

--- onyxia-api/src/main/java/fr/insee/onyxia/api/dao/universe/CatalogRefresher.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/dao/universe/CatalogRefresher.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeoutException;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
 @Service
 public class CatalogRefresher implements ApplicationRunner {
@@ -38,31 +36,6 @@ public class CatalogRefresher implements ApplicationRunner {
                                 logger.info(
                                         helmRepoService.addHelmRepo(c.getLocation(), c.getId()));
                                 catalogLoader.updateCatalog(c);
-                                if (c.getCatalog() != null
-                                        && !CollectionUtils.isEmpty(c.getCatalog().getPackages())) {
-                                    c.getCatalog()
-                                            .setPackages(
-                                                    c.getCatalog().getPackages().stream()
-                                                            .filter(
-                                                                    pkg -> {
-                                                                        if (c.getExcludedCharts()
-                                                                                .contains(
-                                                                                        pkg
-                                                                                                .getName())) {
-                                                                            logger.info(
-                                                                                    "Ignoring chart "
-                                                                                            + pkg
-                                                                                                    .getName()
-                                                                                            + " in catalog "
-                                                                                            + c
-                                                                                                    .getName());
-                                                                            return false;
-                                                                        }
-                                                                        return true;
-                                                                    })
-                                                            .collect(Collectors.toList()));
-                                }
-
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
