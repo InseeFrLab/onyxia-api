d7d3b26439f09071a6ec3d7c776e04c28ef0d192
Olivier Levitt <olivier.levitt@gmail.com>
2023-03-02 10:15:50 +0100

    Keycloak to oidc RC

--- onyxia-api/src/main/java/fr/insee/onyxia/api/security/OIDCConfiguration.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/security/OIDCConfiguration.java
@@ -1,21 +1,41 @@
 package fr.insee.onyxia.api.security;
 
 import fr.insee.onyxia.api.services.UserProvider;
+import fr.insee.onyxia.api.services.utils.HttpRequestUtils;
 import fr.insee.onyxia.model.User;
 import fr.insee.onyxia.model.region.Region;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
 @ConditionalOnProperty(name = "authentication.mode", havingValue = "openidconnect")
 public class OIDCConfiguration {
+
+    @Value("${oidc.username-claim}")
+    private String usernameClaim;
+
+    @Value("${oidc.groups-claim}")
+    private String groupsClaim;
+
+    @Autowired
+    private HttpRequestUtils httpRequestUtils;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -76,11 +96,41 @@ public class OIDCConfiguration {
         return new NullAuthenticatedSessionStrategy();
     }
 
+    @Scope(scopeName = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.NO)
+    public Jwt getUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+        return jwt;
+    }
+
     @Bean
     public UserProvider getUserProvider() {
         return (Region region) -> {
             final User user = User.newInstance().build();
+            Jwt userInfo = getUserInfo();
+            user.setIdep(userInfo.getClaims().get(usernameClaim).toString());
+            user.setIp(
+                    httpRequestUtils.getClientIpAddressIfServletRequestExist(
+                            ((ServletRequestAttributes)
+                                    RequestContextHolder
+                                            .currentRequestAttributes())
+                                    .getRequest()));
+            user.setEmail(userInfo.getClaimAsString("email"));
+            user.setNomComplet(userInfo.getClaimAsString("name"));
+            user.setGroups(userInfo.getClaimAsStringList(groupsClaim));
             return user;
         };
+    }
+
+    public String getUsernameClaim() {
+        return usernameClaim;
+    }
+
+    public void setUsernameClaim(String usernameClaim) {
+        this.usernameClaim = usernameClaim;
+    }
+
+    public void setHttpRequestUtils(HttpRequestUtils httpRequestUtils) {
+        this.httpRequestUtils = httpRequestUtils;
     }
 }
