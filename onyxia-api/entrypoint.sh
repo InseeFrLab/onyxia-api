61ab970db5deb08ef7eac55e2343f52a32650c94
Olivier Levitt <olivier.levitt@gmail.com>
2023-03-17 17:01:57 +0100

    Fix keytool error at startup

--- onyxia-api/entrypoint.sh
+++ onyxia-api/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Import CA certificates to Java keystore
-for file in $CACERTS_DIR/*
-do
-  echo "Adding $file to keystore"
-  keytool -import -cacerts -trustcacerts -noprompt -storepass changeit -alias $(basename $file) -file $file
-done
+if [[ -n "$CACERTS_DIR" ]]; then
+  for file in $CACERTS_DIR/*
+  do
+    echo "Adding $file to keystore"
+    keytool -import -cacerts -trustcacerts -noprompt -storepass changeit -alias $(basename $file) -file $file
+  done
+fi
 
 # Run application
 java -jar /app.jar
