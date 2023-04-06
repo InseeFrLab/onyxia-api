af213f7e31343a5ec58932741d4020d6232d9da3
Olivier Levitt <olivier.levitt@gmail.com>
2023-04-06 14:46:09 +0200

    Fix spotless error

--- onyxia-model/src/main/java/fr/insee/onyxia/model/region/Region.java
+++ onyxia-model/src/main/java/fr/insee/onyxia/model/region/Region.java
@@ -5,10 +5,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import fr.insee.onyxia.model.service.Service;
 import fr.insee.onyxia.model.service.quota.Quota;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Schema(description = "")
 public class Region {
@@ -185,8 +185,8 @@ public class Region {
         private Service.ServiceType type;
         private boolean singleNamespace = true;
         private boolean allowNamespaceCreation = true;
-        private Map<String,String> namespaceLabels = new HashMap<String,String>();
-        private Map<String,String> namespaceAnnotations = new HashMap<String,String>();
+        private Map<String, String> namespaceLabels = new HashMap<String, String>();
+        private Map<String, String> namespaceAnnotations = new HashMap<String, String>();
         private boolean userNamespace = true;
         private String namespacePrefix = "user-";
         private String groupNamespacePrefix = "projet-";
@@ -512,19 +512,19 @@ public class Region {
             this.allowNamespaceCreation = allowNamespaceCreation;
         }
 
-        public Map<String,String> getNamespaceLabels() {
+        public Map<String, String> getNamespaceLabels() {
             return namespaceLabels;
         }
 
-        public void getNamespaceLabels(Map<String,String> namespaceLabels) {
+        public void getNamespaceLabels(Map<String, String> namespaceLabels) {
             this.namespaceLabels = namespaceLabels;
         }
 
-        public Map<String,String> getNamespaceAnnotations() {
+        public Map<String, String> getNamespaceAnnotations() {
             return namespaceAnnotations;
         }
 
-        public void getNamespaceAnnotations(Map<String,String> namespaceAnnotations) {
+        public void getNamespaceAnnotations(Map<String, String> namespaceAnnotations) {
             this.namespaceAnnotations = namespaceAnnotations;
         }
 
