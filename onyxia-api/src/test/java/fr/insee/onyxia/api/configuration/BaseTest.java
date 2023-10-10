3b47b01133360ddbfc49bfa9950ef1449ef6f07f
Olivier Levitt <olivier.levitt@gmail.com>
2023-10-10 14:44:40 +0200

    Fix test

--- onyxia-api/src/test/java/fr/insee/onyxia/api/configuration/BaseTest.java
+++ onyxia-api/src/test/java/fr/insee/onyxia/api/configuration/BaseTest.java
@@ -1,11 +1,14 @@
 package fr.insee.onyxia.api.configuration;
 
+import fr.insee.onyxia.api.security.NoSecurityConfiguration;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @AutoConfigureRestDocs(outputDir = "target/snippets")
 @TestPropertySource("classpath:application-test.properties")
+@Import(NoSecurityConfiguration.class)
 public abstract class BaseTest {}
