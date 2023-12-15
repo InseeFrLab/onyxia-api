320503cdbbc9f0caa84b384f791c1442cfb0856f
Ines Hiverlet <ines.hiverlet@insee.fr>
2023-12-15 11:47:23 +0100

    wip

--- helm-wrapper/src/main/java/io/github/inseefrlab/helmwrapper/model/HelmInstaller.java
+++ helm-wrapper/src/main/java/io/github/inseefrlab/helmwrapper/model/HelmInstaller.java
@@ -1,11 +1,10 @@
 package io.github.inseefrlab.helmwrapper.model;
 
 import java.util.ArrayList;
-import org.springframework.stereotype.Component;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.SafeConstructor;
 
-@Component
+
 public class HelmInstaller {
 
     private String name;
