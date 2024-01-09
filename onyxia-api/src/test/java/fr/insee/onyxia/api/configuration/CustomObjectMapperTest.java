012783d15494d332dc9129cbfc2f7517883cbde7
John Kasper Svergja <johnkasper.svergja@ssb.no>
2024-01-09 11:00:40 +0100

    Cleanup code
    Remove public modifier that is not necessary for test

--- onyxia-api/src/test/java/fr/insee/onyxia/api/configuration/CustomObjectMapperTest.java
+++ onyxia-api/src/test/java/fr/insee/onyxia/api/configuration/CustomObjectMapperTest.java
@@ -11,7 +11,7 @@ class CustomObjectMapperTest {
     CustomObjectMapper customObjectMapper = new CustomObjectMapper();
 
     @Test
-    public void objectMapperShouldParseJson5IshDocument() {
+    void objectMapperShouldParseJson5IshDocument() {
         var mapper = customObjectMapper.objectMapper();
         var catalogsJson = getClassPathResource("catalog-loader-test/catalogs.json5");
         var catalogs = assertDoesNotThrow(() -> mapper.readValue(catalogsJson, Catalogs.class));
