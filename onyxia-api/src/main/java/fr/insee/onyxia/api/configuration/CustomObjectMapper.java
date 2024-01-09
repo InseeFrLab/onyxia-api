58a4c3956de6a3323f999e99a99b2a3d6b0b60c0
John Kasper Svergja <johnkasper.svergja@ssb.no>
2024-01-09 09:43:19 +0100

    Move to JsonMapper builder for new object mappers
    Go away from deprecated usage of objectmapper, and move to new method where builder object is used

--- onyxia-api/src/main/java/fr/insee/onyxia/api/configuration/CustomObjectMapper.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/configuration/CustomObjectMapper.java
@@ -2,6 +2,7 @@ package fr.insee.onyxia.api.configuration;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -14,24 +15,22 @@ public class CustomObjectMapper {
     @Bean
     @Primary
     public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
+        JsonMapper.Builder mapper = JsonMapper.builder();
         commonConfiguration(mapper);
-        return mapper;
+        return mapper.build();
     }
 
     @Bean
     @Qualifier("helm")
     public ObjectMapper objectMapperHelm() {
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-
+        JsonMapper.Builder mapper = JsonMapper.builder(new YAMLFactory());
         commonConfiguration(mapper);
-
-        return mapper;
+        return mapper.build();
     }
 
-    private void commonConfiguration(ObjectMapper mapper) {
+    private void commonConfiguration(JsonMapper.Builder mapper) {
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.setSerializationInclusion(Include.NON_NULL);
+        mapper.serializationInclusion(Include.NON_NULL);
         mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
         mapper.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
