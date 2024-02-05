76a4fdac1cd7e327306e9b09193bdb8d689afe82
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2024-02-05 18:28:50 +0100

    Improve readiness checks (#353)

--- onyxia-api/src/main/java/fr/insee/onyxia/api/configuration/properties/CatalogsConfiguration.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/configuration/properties/CatalogsConfiguration.java
@@ -8,8 +8,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -27,13 +25,11 @@ import org.springframework.core.io.support.PropertySourceFactory;
 @ConfigurationProperties
 public class CatalogsConfiguration {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CatalogsConfiguration.class);
-
     private String catalogs;
 
     private List<CatalogWrapper> resolvedCatalogs;
 
-    private final ObjectMapper mapper;
+    private ObjectMapper mapper;
 
     @Autowired
     public CatalogsConfiguration(ObjectMapper mapper) {
@@ -43,7 +39,6 @@ public class CatalogsConfiguration {
     @PostConstruct
     public void load() throws Exception {
         resolvedCatalogs = Arrays.asList(mapper.readValue(catalogs, CatalogWrapper[].class));
-        LOGGER.info("Serving {} catalogs", resolvedCatalogs.size());
     }
 
     public List<CatalogWrapper> getResolvedCatalogs() {
