5216680f2d558b72fcdb525a1266aad4295972b7
Olivier Levitt <olivier.levitt@gmail.com>
2024-02-09 11:09:26 +0100

    Fix spotless

--- onyxia-api/src/main/java/fr/insee/onyxia/api/controller/pub/ConfigurationController.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/controller/pub/ConfigurationController.java
@@ -6,14 +6,13 @@ import fr.insee.onyxia.model.region.Region;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @Tag(name = "Public", description = "Information endpoints")
