f56643581bae14991aac3e6694b66110c9bf1804
Olivier Levitt <olivier.levitt@gmail.com>
2024-02-26 14:35:23 +0100

    Fix swagger auth (#392)

--- onyxia-api/src/main/java/fr/insee/onyxia/api/openapi/OpenApiConfiguration.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/openapi/OpenApiConfiguration.java
@@ -1,28 +1,43 @@
 package fr.insee.onyxia.api.openapi;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class OpenApiConfiguration {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OpenApiConfiguration.class);
 
-    // TODO : add oauth authentication
+    @Value("${oidc.issuer-uri:#{null}}")
+    private String issuerUri;
 
     @Bean
-    @ConditionalOnMissingBean
+    @Primary
     public OpenAPI customOpenAPI() {
-        return createOpenAPI();
+        return baseOpenAPIConfiguration();
     }
 
-    private OpenAPI createOpenAPI() {
-        LOGGER.info("surcharge de la configuration swagger");
+    @Bean
+    @ConditionalOnProperty(name = "authentication.mode", havingValue = "openidconnect")
+    public OpenAPI createOpenAPI() {
+        SecurityScheme scheme =
+                new SecurityScheme()
+                        .type(SecurityScheme.Type.OPENIDCONNECT)
+                        .openIdConnectUrl(issuerUri + "/.well-known/openid-configuration");
+        return baseOpenAPIConfiguration()
+                .components(new Components().addSecuritySchemes("auth", scheme));
+    }
+
+    private OpenAPI baseOpenAPIConfiguration() {
         return new OpenAPI().info(new Info().title("Onyxia-api").description("Swagger onyxia-api"));
     }
 }
