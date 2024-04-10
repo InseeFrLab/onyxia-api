0c46925a57eb59bd71a228fc15c8d4b062ff0774
Olivier Levitt <olivier.levitt@gmail.com>
2024-04-10 11:31:46 +0200

    Cleanup duplication

--- onyxia-api/src/main/java/fr/insee/onyxia/api/events/PauseResumeServiceEvent.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/events/PauseResumeServiceEvent.java
@@ -1,30 +1,8 @@
 package fr.insee.onyxia.api.events;
 
-public class PauseResumeServiceEvent extends OnyxiaEvent {
+public class PauseResumeServiceEvent extends InstallServiceEvent {
 
-    private String type;
-    private String namespace;
-    private String releaseName;
-    private String packageName;
-
-    public String getReleaseName() {
-        return releaseName;
-    }
-
-    public void setReleaseName(String releaseName) {
-        this.releaseName = releaseName;
-    }
-
-    public String getPackageName() {
-        return packageName;
-    }
-
-    public void setPackageName(String packageName) {
-        this.packageName = packageName;
-    }
-
-    private String username;
-    private String catalogId;
+    private boolean isPause;
 
     public PauseResumeServiceEvent() {}
 
@@ -35,44 +13,12 @@ public class PauseResumeServiceEvent extends OnyxiaEvent {
             String packageName,
             String catalogId,
             boolean pause) {
-        this.namespace = namespace;
-        this.releaseName = releaseName;
-        this.catalogId = catalogId;
-        this.username = username;
-        this.packageName = packageName;
-        if (pause) {
-            type = "service.pause";
-        } else {
-            type = "service.resume";
-        }
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getCatalogId() {
-        return catalogId;
-    }
-
-    public void setCatalogId(String catalogId) {
-        this.catalogId = catalogId;
-    }
-
-    public String getNamespace() {
-        return namespace;
-    }
-
-    public void setNamespace(String namespace) {
-        this.namespace = namespace;
+        super(username, namespace, releaseName, packageName, catalogId);
+        this.isPause = pause;
     }
 
     @Override
     public String getType() {
-        return type;
+        return isPause ? "service.pause" : "service.install";
     }
 }
