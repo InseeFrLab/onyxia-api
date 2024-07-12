3711858431cf75e10332fe9685fc2b62d0dd53cc
Comte <frederic.comte@insee.fr>
2024-07-12 13:51:29 +0200

    add annotation postconstruct pom

--- onyxia-api/src/main/java/fr/insee/onyxia/api/controller/exception/SchemaNotFoundException.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/controller/exception/SchemaNotFoundException.java
@@ -1,3 +1,5 @@
+package fr.insee.onyxia.api.controller.exception;
+
 public class SchemaNotFoundException extends RuntimeException {
 
     public SchemaNotFoundException(String schemaName) {
--- onyxia-api/src/main/java/fr/insee/onyxia/api/controller/pub/JsonSchemaController.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/controller/pub/JsonSchemaController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import fr.insee.onyxia.api.services.JsonSchemaRegistryService;
-
+import fr.insee.onyxia.api.controller.exception.SchemaNotFoundException; 
 import java.util.Map;
 
 @RestController
