deea725b615cabd66506b4a65eb0539eb3f581b8
Comte <frederic.comte@insee.fr>
2024-07-12 13:30:23 +0200

    Update RestExceptionHandler.java

--- onyxia-api/src/main/java/fr/insee/onyxia/api/controller/RestExceptionHandler.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/controller/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package fr.insee.onyxia.api.controller;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
