7a0f4ac25b264f19cd111b9517d33ad17b4e6a47
fcomte <frederic.comte@insee.fr>
2024-07-25 08:26:52 +0000

    handle validation exception

--- onyxia-api/src/main/java/fr/insee/onyxia/api/controller/RestExceptionHandler.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/controller/RestExceptionHandler.java
@@ -1,6 +1,9 @@
 package fr.insee.onyxia.api.controller;
 
 import fr.insee.onyxia.api.controller.exception.SchemaNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.everit.json.schema.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -18,5 +21,18 @@ public class RestExceptionHandler {
     @ExceptionHandler(SchemaNotFoundException.class)
     public ResponseEntity<String> handleSchemaNotFoundException(SchemaNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(ValidationException ex) {
+        List<String> errors =
+                ex.getCausingExceptions().stream()
+                        .map(ValidationException::getMessage)
+                        .collect(Collectors.toList());
+
+        ErrorResponse errorResponse =
+                new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Validation failed", errors);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
