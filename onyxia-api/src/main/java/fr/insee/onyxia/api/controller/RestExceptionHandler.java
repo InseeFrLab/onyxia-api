0bac175a730588b1e0b9688d58fa0cc243bf0356
Olivier Levitt <olivier.levitt@gmail.com>
2024-09-02 09:59:06 +0200

    Use JSON schema for configuration (#455)
    Co-authored-by: Comte <frederic.comte@insee.fr>
    Co-authored-by: ihiverlet <94982379+ihiverlet@users.noreply.github.com>

--- onyxia-api/src/main/java/fr/insee/onyxia/api/controller/RestExceptionHandler.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/controller/RestExceptionHandler.java
@@ -1,6 +1,11 @@
 package fr.insee.onyxia.api.controller;
 
+import fr.insee.onyxia.api.controller.exception.SchemaNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.everit.json.schema.ValidationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,4 +17,59 @@ public class RestExceptionHandler {
     @ResponseStatus(value = HttpStatus.FORBIDDEN)
     @ExceptionHandler(AccessDeniedException.class)
     public void handleAccessDeniedException(Exception ignored) {}
+
+    @ExceptionHandler(SchemaNotFoundException.class)
+    public ResponseEntity<String> handleSchemaNotFoundException(SchemaNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(ValidationException ex) {
+        List<String> errors =
+                ex.getCausingExceptions().stream()
+                        .map(ValidationException::getMessage)
+                        .collect(Collectors.toList());
+
+        ErrorResponse errorResponse =
+                new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Validation failed", errors);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    // Define the ErrorResponse class within the GlobalExceptionHandler
+    public static class ErrorResponse {
+        private int status;
+        private String message;
+        private List<String> errors;
+
+        public ErrorResponse(int status, String message, List<String> errors) {
+            this.status = status;
+            this.message = message;
+            this.errors = errors;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public List<String> getErrors() {
+            return errors;
+        }
+
+        public void setErrors(List<String> errors) {
+            this.errors = errors;
+        }
+    }
 }
