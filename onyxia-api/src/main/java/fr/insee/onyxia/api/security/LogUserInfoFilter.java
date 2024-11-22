5c84c596045b78c2b5c198dcc35708115017127f
Olivier Levitt <olivier.levitt@gmail.com>
2024-11-22 11:11:39 +0100

    Spotless fix

--- onyxia-api/src/main/java/fr/insee/onyxia/api/security/LogUserInfoFilter.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/security/LogUserInfoFilter.java
@@ -6,26 +6,26 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-
 @Component
 public class LogUserInfoFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private UserProvider userProvider;
+    @Autowired private UserProvider userProvider;
 
-    @Autowired
-    private RegionsConfiguration regionsConfiguration;
+    @Autowired private RegionsConfiguration regionsConfiguration;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
-        MDC.put("username", userProvider.getUser(regionsConfiguration.getDefaultRegion()).getIdep());
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        MDC.put(
+                "username",
+                userProvider.getUser(regionsConfiguration.getDefaultRegion()).getIdep());
         filterChain.doFilter(request, response);
         MDC.clear();
     }
