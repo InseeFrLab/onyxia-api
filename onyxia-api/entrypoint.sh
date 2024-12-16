e8ab362c0150fbd126c04a5b4aa95de695b9fc66
Olivier Levitt <olivier.levitt@gmail.com>
2024-12-16 16:30:17 +0100

    Allow JMX debug

--- onyxia-api/entrypoint.sh
+++ onyxia-api/entrypoint.sh
@@ -13,4 +13,9 @@ if [[ -n "$CACERTS_DIR" ]]; then
 fi
 
 # Run application
-java org.springframework.boot.loader.launch.JarLauncher
+if [ -n "$DEBUG_JMX" ]; then
+  JMX_PORT="${JMX_PORT:-10000}"
+  java -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.local.only=false org.springframework.boot.loader.launch.JarLauncher
+else
+  java org.springframework.boot.loader.launch.JarLauncher
+fi
